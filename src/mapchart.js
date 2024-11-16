dc84e8b3debda9468955667d67654fc99d2c2f8c
nora chams <nchamseddin@gmail.com>
2024-11-16 03:00:28 -0500

    added city marker

--- src/mapchart.js
+++ src/mapchart.js
@@ -3,7 +3,8 @@ import {
   ComposableMap,
   Geographies,
   Geography,
-  ZoomableGroup
+  ZoomableGroup,
+  Marker
 } from "react-simple-maps";
 import { Text, Box, Input, Button, VStack, HStack } from "@chakra-ui/react"; // Import Chakra UI components
 import Menu from "./menu";
@@ -20,8 +21,10 @@ const MapChart = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredCities, setFilteredCities] = useState([]);
   const [selectedCity, setSelectedCity] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [hoveredMarkerIndex, setHoveredMarkerIndex] = useState(null);
 
-   
+
    const handleSearchChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
@@ -39,26 +42,41 @@ const MapChart = () => {
   const handleCitySelect = (city) => {
     setSelectedCity(city);
     setFilteredCities([]);
-    setSearchQuery(city.name);
+    setSearchQuery("");
   };
 
   const confirmAddCity = () => {
     if (selectedCity) {
       const { country, lat, lon } = selectedCity;
+      const latNum = parseFloat(lat);
+      const lonNum = parseFloat(lon);
+
       if (!visitedCountries.includes(country)) {
         setVisitedCountries((prev) => [...prev, country]);
       }
-      setSelectedCity(null);
+
+      // Add the city's coordinates to the markers state
+    setMarkers((prevMarkers) => [
+      ...prevMarkers,
+      { coordinates: [lonNum, latNum], name: selectedCity.name },
+    ]);
+
+
       alert(`${selectedCity.name} has been added to your visited cities.`);
+
+      setSelectedCity(null);
+      
     }
   };
 
   const handleCountryClick = (geo) => {
+      setVisitedCountries((prev) =>
+        prev.includes(geo.id) ? prev.filter((id) => id !== geo.id) : [...prev, geo.id]
+      );
+    }; 
+
     
-    setVisitedCountries((prev) =>
-      prev.includes(geo.id) ? prev.filter((id) => id !== geo.id) : [...prev, geo.id]
-    );
-  };
+
 
   const cycleDisplayMode = () => {
     setDisplayMode((prevMode) => (prevMode + 1) % 3);
@@ -82,8 +100,7 @@ const MapChart = () => {
     <div>
      <Box position="fixed" top="10px" width="100%" textAlign="center">
         <Text
-          //onClick={cycleDisplayMode}
-          onClick={() => setDisplayMode((prev) => (prev + 1) % 2)}
+          onClick={cycleDisplayMode}
           fontSize="2xl"
           fontWeight="bold"
           color="teal.500"
@@ -97,19 +114,21 @@ const MapChart = () => {
       </Box>
       <Menu />
       {/* Search bar and dropdown */}
-      <Box position="fixed" top="18px" right="20px" width="200px" borderColor="white" >
+      <Box position="fixed" top="15px" right="20px" width="250px" borderColor="white" >
         <Input
-          placeholder="    Search for a city you visted..."
+          placeholder="Search for a city you visted..."
           value={searchQuery}
           onChange={handleSearchChange}
           mb={2}
           borderColor="white"
+          p={4}
           bg={"white"}
           borderRadius="sm"
           shadow={"md"}
+          size="lg"
           
         />
-        <VStack align="start" spacing={1} bg="white" borderRadius="md" shadow="md" maxHeight="200px" overflow="auto">
+        <VStack align="start" spacing={3} bg="white" borderRadius="md" shadow="md" maxHeight="200px" overflow="auto">
           {filteredCities.map((city) => (
             <Button
               key={city.id}
@@ -117,6 +136,7 @@ const MapChart = () => {
               size="sm"
               variant="ghost"
               width="100%"
+              justifyContent="flex-start" //hello added this
             >
               {city.name}, {city.country}
             </Button>
@@ -135,7 +155,8 @@ const MapChart = () => {
           p={4}
           borderRadius="md"
           shadow="lg"
-          textAlign="center"
+          textAlign="center" 
+          zIndex="1000" //added this too 
         >
           <Text mb={4}>Do you want to add {selectedCity.name} to your visited list?</Text>
           <HStack justify="center">
@@ -153,6 +174,7 @@ const MapChart = () => {
           <Geographies geography="/feature.json">
             {({ geographies }) =>
               geographies.map((geo) => {
+                const countryCode = geo.properties.ISO_A2;
                 const isSelected = visitedCountries.includes(geo.id); // Check if country is selected
                 return (
                   <Geography
@@ -183,8 +205,28 @@ const MapChart = () => {
               })
             }
           </Geographies>
+
+      {markers.map((marker, i) => (
+      <Marker key={i} coordinates={marker.coordinates} onMouseEnter={() => setHoveredMarkerIndex(i)}
+      onMouseLeave={() => setHoveredMarkerIndex(null)}>
+        <circle r={0.5} fill="#42C4DB"  />
+        {hoveredMarkerIndex === i && (
+        <text
+          textAnchor="middle"
+          y={-3}
+          style={{ fontFamily: "system-ui", fill: "#5D5A6D", fontSize: "7px",
+            pointerEvents: "none",
+          }}
+          
+        >
+          {marker.name}
+        </text>
+        )}
+      </Marker>
+    ))}
         </ZoomableGroup>
       </ComposableMap>
+
       {/* Hover Box at Bottom Left */}
       {hoveredCountry && (
         <div style={{
